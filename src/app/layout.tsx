5bee49e729aa154821e68bbbb55e5c39d0672e15
Matheus Chignolli <matheuschignolli@gmail.com>
2026-02-09 16:36:54 -0300

    update gtm code

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -73,7 +73,7 @@ export default function RootLayout({
           new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
           j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
           'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-          })(window,document,'script','dataLayer','GTM-NK59FP42');`
+          })(window,document,'script','dataLayer','GTM-WWW5WQM7');`
           }}
         />
         {/* End Google Tag Manager */}
@@ -98,7 +98,7 @@ export default function RootLayout({
         {/* Google Tag Manager (noscript) */}
         <noscript>
           <iframe
-            src="https://www.googletagmanager.com/ns.html?id=GTM-NK59FP42"
+            src="https://www.googletagmanager.com/ns.html?id=GTM-WWW5WQM7"
             height="0"
             width="0"
             style={{ display: 'none', visibility: 'hidden' }}
